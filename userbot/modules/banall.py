b47ef4ef093b6f092155ca5fee47ee268ffccbcf
mrismanaziz <mrismanaziz@gmail.com>
2021-06-14 08:27:25 +0000

    pylint: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/banall.py
+++ userbot/modules/banall.py
@@ -1,14 +1,16 @@
-# Port By @VckyouuBitch 
+# Port By @VckyouuBitch
 # From GeezProject <https://github.com/vckyou/Geez-UserBot>
 # Perkontolan Dengan Hapus Credits
 
 from asyncio import sleep
-from telethon.tl.types import ChatBannedRights
+
 from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import ChatBannedRights
+
 from userbot.events import register
 
 
-@register(outgoing=True, pattern="^\.banall(?: |$)(.*)")
+@register(outgoing=True, pattern="^\\.banall(?: |$)(.*)")
 async def testing(event):
     nikal = await event.get_chat()
     chutiya = await event.client.get_me()
@@ -19,16 +21,21 @@ async def testing(event):
         return
     await event.edit("Tidak Melakukan Apa-apa")
 
-# Thank for Dark_Cobra
+    # Thank for Dark_Cobra
 
     everyone = await event.client.get_participants(event.chat_id)
     for user in everyone:
         if user.id == chutiya.id:
             pass
         try:
-            await event.client(EditBannedRequest(event.chat_id, int(user.id), ChatBannedRights(until_date=None, view_messages=True)))
+            await event.client(
+                EditBannedRequest(
+                    event.chat_id,
+                    int(user.id),
+                    ChatBannedRights(until_date=None, view_messages=True),
+                )
+            )
         except Exception as e:
             await event.edit(str(e))
-        await sleep(.5)
+        await sleep(0.5)
     await event.edit("Tidak Ada yang Terjadi di sini🙃🙂")
-
